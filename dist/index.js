200a25e9ad7d8eaaacba43df6959d0c6dd67c558
Adrian Helvik <adrian@adrian.local>
2018-08-27 15:52:58 +0200

    ...

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _convertSyntax = require('./convertSyntax');
+
+var _convertSyntax2 = _interopRequireDefault(_convertSyntax);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _convertSyntax2.default;
